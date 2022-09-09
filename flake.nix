a2dd27e57115ba2fadba2aff3dd7de19734488df
Hugo Sum <hugosum@Hugos-MacBook-Pro.local>
2022-09-09 20:52:25 +0100

    New signature

--- flake.nix
+++ flake.nix
@@ -1,39 +1,49 @@
 {
   description = "closesource repo flake";
 
+  # main
   inputs = {
-    nixpkgs = { url = "github:nixos/nixpkgs/release-22.05"; };
-
-    deno2nix = { url = "github:SnO2WMaN/deno2nix/main"; };
-
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-      inputs = { nixpkgs.follows = "nixpkgs"; };
-    };
+    nixpkgs = {url = "github:nixos/nixpkgs/release-22.05";};
+    deno2nix = {url = "github:SnO2WMaN/deno2nix/main";};
   };
 
-  outputs = { self, nixpkgs, flake-utils, deno2nix, ... }:
-      flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = (import nixpkgs {
-          inherit system;
-          overlays = [
-            deno2nix.overlay
-          ];
-        });
+  # dev
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-          deno-api-dynamic-form = pkgs.deno2nix.mkExecutable {
-            name = "deno-api-dynamic-form";
-            version = "0.0.0";
-            src = ./.;
-            denoFlags = ["--allow-net" "--allow-env" "--allow-read" "--import-map=./import_map.json"];
-            lockfile = ./lock.json;
-            entrypoint = ./src/index.ts;
-          };
-      in {
-        # nix build .#<appName>
-        packages = {
-          inherit deno-api-dynamic-form;
-        };
-      });
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    deno2nix,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          deno2nix.overlay
+        ];
+      };
+
+      deno-api-dynamic-form = pkgs.deno2nix.mkExecutable {
+        pname = "deno-api-dynamic-form";
+        version = "0.0.0";
+
+        src = ./.;
+        lockfile = ./lock.json;
+
+        output = "deno-api-dynamic-form";
+        entrypoint = "./src/index.ts";
+        importMap = "./import_map.json";
+        additionalDenoFlags = "--allow-net --allow-env --allow-read";
+      };
+    in {
+      # nix build .#<appName>
+      packages = {
+        inherit deno-api-dynamic-form;
+      };
+      apps.default = flake-utils.lib.mkApp {drv = self.packages.${system}.deno-api-dynamic-form;};
+    });
 }
