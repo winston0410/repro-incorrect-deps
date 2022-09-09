a9115cc4623a1c4416d6861073a229487f09c00f
Hugo Sum <hugosum@Hugos-MacBook-Pro.local>
2022-09-09 19:48:14 +0100

    Fix build

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import * as sinon from "https://cdn.skypack.dev/sinon@v14.0.0";
-import { message} from './app.ts'
+import { message} from '@project/src/app.ts'
 
 console.log('hello world', sinon, message)
